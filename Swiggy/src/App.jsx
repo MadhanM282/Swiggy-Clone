fcbd0af342cc0c056ca6b73e24045e1391b9cf98
MadhanM282 <madhanm282@gmail.com>
2022-03-30 08:48:21 +0530

    NAv and Login

--- Swiggy/src/App.jsx
+++ Swiggy/src/App.jsx
@@ -3,18 +3,22 @@ import { Route,  Routes } from 'react-router-dom'
 import './App.css'
 import { Cart } from './components/cartPage'
 import { Home } from './components/landingPAge'
-import { NavBar } from './components/NavBar'
+import { SignInSide } from './components/login'
+import { ResponsiveAppBar } from './components/NavBar'
+// import { NavBar } from './components/NavBar'
 import { Restaurent } from './components/Restaurentpage'
 
 function App() {
 
   return (
     <div className="App">
-      <NavBar/>
+      {/* <NavBar/> */}
+      <ResponsiveAppBar/>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/cart" element={<Cart/>}/>
         <Route path="/restaurent" element={<Restaurent/>}/>
+        <Route path="/login" element={<SignInSide/>}/>
       </Routes>
     </div>
   )
--- Swiggy/src/components/NavBar.jsx
+++ Swiggy/src/components/NavBar.jsx
@@ -1,22 +1,144 @@
-import { Link } from "react-router-dom"
-import { NAVDIV, NavSec2 } from "../Styles/homestyles"
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import { Link, useNavigate } from 'react-router-dom';
+import {SignInSide} from './login';
 
-export const NavBar= ()=>{
-    return(
-        <NAVDIV>
-            <div>
-                <h2>location</h2>
-            </div>
-            <NavSec2>
-                <h2>search</h2>
-                <h2>Offers</h2>
-                <h2>Help</h2>
-                <h2>Sign in</h2>
-                <Link to="/cart">
-                    <h2>Cart</h2>
-                </Link>
-            </NavSec2>
-        
-        </NAVDIV>
-    )
-}
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+export const ResponsiveAppBar = () => {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+    const navigate = useNavigate()
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+                    >
+                        LOGO
+                    </Typography>
+
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center">{page}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+                    >
+                        LOGO
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page}
+                                onClick={handleCloseNavMenu}
+                                sx={{ my: 2, color: 'white', display: 'block' }}
+                            >
+                                {page}
+                            </Button>
+                        ))}
+                    </Box>
+
+                    <Box sx={{ flexGrow: 0 }}>
+                        {/* <Link to={'/'}> */}
+                            <Tooltip title="Login" >
+                                <IconButton onClick={()=>navigate("/login")} sx={{ p: 0 }}>
+                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                                </IconButton>
+                            </Tooltip>
+                        {/* </Link> */}
+                        {/* <Menu
+                            sx={{ mt: '45px' }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.map((setting) => (
+                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                    <Typography textAlign="center">{setting}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu> */}
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+};
