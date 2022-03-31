309d1565b98ad5f4057d95cdc172eee82915907c
iamrituyadav <iamrituyadav@outlook.com>
2022-03-31 18:08:09 +0530

    initial commit in Restaurenpage

--- Swiggy/src/App.jsx
+++ Swiggy/src/App.jsx
@@ -1,27 +1,25 @@
-
-import { Route,  Routes } from 'react-router-dom'
-import './App.css'
-import { Cart } from './components/cartPage'
-import { Home } from './components/landingPAge'
-import { SignInSide } from './components/login'
-import { ResponsiveAppBar } from './components/NavBar'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Cart } from "./components/cartPage";
+import { Home } from "./components/landingPAge";
+import { SignInSide } from "./components/login";
+import { ResponsiveAppBar } from "./components/NavBar";
 // import { NavBar } from './components/NavBar'
-import { Restaurent } from './components/Restaurentpage'
+import { Restaurent } from "./components/RestaurantPage/Restaurentpage";
 
 function App() {
-
   return (
     <div className="App">
       {/* <NavBar/> */}
-      <ResponsiveAppBar/>
+      <ResponsiveAppBar />
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/cart" element={<Cart/>}/>
-        <Route path="/restaurent" element={<Restaurent/>}/>
-        <Route path="/login" element={<SignInSide/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/restaurent" element={<Restaurent />} />
+        <Route path="/login" element={<SignInSide />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Swiggy/src/components/RestaurantPage/Restaurentpage.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+import { makeStyles } from "@mui/styles";
+
+// import { useStyles } from "../../Styles/Restaurentpage";
+import HeaderImg from "./HeaderImg";
+import RestaurantName from "./RestaurantInfo";
+import OfferHeader from "./OfferHeader";
+import ItemType from "./ItemType";
+import ItemDisplay from "./ItemDisplay";
+
+const useStyles = makeStyles({
+  Wrapper: {
+    display: "flex",
+    padding: "16px 100px ",
+    background: "#171a29",
+    color: "white",
+  },
+  RestaurantNameWrapper: {
+    margin: "0 55px",
+    color: "white",
+  },
+
+  LowerDivWrapper: {
+    display: "flex",
+  },
+});
+
+export const Restaurent = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <div className={`${classes.Wrapper}`}>
+        <HeaderImg />
+        <RestaurantName
+          className={`${classes.RestaurantNameWrapper}`}
+          name="Om Sweet Shop"
+          cusines="North Indian, Italian"
+          address="KB Nagar, XYZ"
+          infoArray="[1,2,3]"
+        />
+        <OfferHeader />
+      </div>
+      <div className={`${classes.LowerDivWrapper}`}>
+        <ItemType item={[1, 2, 3, 4, 5]} />
+        <ItemDisplay
+          type="veg"
+          name="Razma Chawal"
+          price="400"
+          image="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/bmwn4n4bn6n1tcpc8x2h"
+        />
+      </div>
+      <div></div>
+    </div>
+  );
+};
