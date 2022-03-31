f81f71b0c9bb4f1ecb3eb4c0a02b9351354903a3
MadhanM282 <madhanm282@gmail.com>
2022-03-31 11:38:32 +0530

    cartstructure

--- Swiggy/src/components/CART/cartPage.jsx
+++ Swiggy/src/components/CART/cartPage.jsx
@@ -4,7 +4,7 @@ import { Empty } from "./Empty";
 
 export const Cart= ()=>{
     const {cart,login,address,payment} = useSelector((store)=>store.cart)
-    const len = cart.length
+    const len = 0 // cart.length
     console.log('cart', cart);
     return len===0?<Empty/>:(
         <Main>
