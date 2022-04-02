b421b89a624449bb5e40c84eda299ed9aef752a1
MadhanM282 <madhanm282@gmail.com>
2022-04-02 17:24:25 +0530

    total

--- Swiggy/src/ReduxFiles/cart/cartAction.js
+++ Swiggy/src/ReduxFiles/cart/cartAction.js
@@ -2,6 +2,8 @@
 export const LOG_STAT = "LOG_STAT";
 export const ADDRESS_STAT = "ADDRESS_STAT";
 export const PAYMENT_STAT = "PAYMENT_STAT";
+export const RESIMG = "RESIMG";
+export const RESNM = "RESNM";
 
 
 
@@ -11,5 +13,9 @@ export const AddressStat = () => ({ type: ADDRESS_STAT });
 
 export const PaymentStat = () => ({ type: PAYMENT_STAT });
 
+export const ResImg= (data)=>({type:RESIMG,payload:data})
 
-// export const CartRemove = (data)=>({type:CART,payload:data});
+export const Resname= (data)=>({type:RESNM,payload:data})
+
+
+
