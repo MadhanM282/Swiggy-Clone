31ff42b5935728ffb223ab336ed6290eff5b09d3
iamrituyadav <iamrituyadav@outlook.com>
2022-04-03 13:47:00 +0530

    discount icon added

--- Swiggy/src/components/RestaurantPage/OfferHeader.jsx
+++ Swiggy/src/components/RestaurantPage/OfferHeader.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { OfferHeaderDiv } from "../../Styles/RestaurantPage";
+import DiscountIcon from "../../../Icons/discount.svg";
+import { IconImg, OfferHeaderDiv } from "../../Styles/RestaurantPage";
 
 export default function OfferHeader() {
   return (
     <OfferHeaderDiv>
       <h4>Offer</h4>
-      <h6>20% off up to ₹125 | Use KOTAK125 Above ₹500</h6>
+      <h6>
+        <IconImg src={DiscountIcon} />  20% off up to ₹125 | Use KOTAK125 Above
+        ₹500`
+      </h6>
     </OfferHeaderDiv>
   );
 }
